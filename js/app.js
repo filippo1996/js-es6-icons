93b67e4aea2fab6547e098d6170a05e7d7056e49
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-06-18 22:32:39 +0200

    add logic color + filter

--- js/app.js
+++ js/app.js
@@ -103,12 +103,96 @@ const icons = [
     },
   ];
 
+const eleShowIcon = document.getElementById('show-icon');
 
-icons.forEach(ele => {
-    document.getElementById('show-icon').innerHTML += 
-    `<div class="col-2 p-35">
-        <div class="card">
-            <i class="${ele.family} ${ele.prefix}${ele.name}"></i><span class="name-icon">${ele.name}</span>
-        </div>
-    </div>`;
+/**
+ * Funzione che stampa gli oggetti nell'html
+ * @param {array} array 
+ * @param {object} elementHTML 
+ */
+function print(array ,elementHTML){
+  elementHTML.innerHTML = '';
+    array.forEach(ele => {
+        elementHTML.innerHTML += 
+        `<div class="col-2 p-35">
+            <div class="card">
+                <i class="${ele.family} ${ele.prefix}${ele.name}" style="color:${ele.color}"></i><span class="name-icon">${ele.name}</span>
+            </div>
+        </div>`;
+    });
+}
+
+
+
+/**
+ * Milestone 2 
+ * Coloriamo le icone per tipo
+ */
+/*
+const addPropertyObj = (array, ...property) => {
+    return array.map(ele => {
+        const obj = 
+           {
+               ...ele
+                
+           };
+        
+        return obj;
+    });
+};
+
+console.log(addPropertyObj(icons, 'color'));
+*/
+
+const addColorObj = icons.map(ele => {
+    let color = '';
+    ele.type === 'animal' ? color = 'red' 
+    : ele.type === 'vegetable' ? color = 'green'
+    : color = 'purple';
+
+    const obj = 
+       {
+           ...ele,
+           color
+       };
+    
+    return obj;
 });
+
+print(addColorObj ,eleShowIcon);
+
+
+/**
+ * Milestone 3 
+ * Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+ */
+
+
+//Mostriamo i filtri menu nell'html
+getElementHtmlFilter(icons);
+
+document.getElementById('filter-selected').addEventListener('change', function(){
+  const itemsFilter = addColorObj.filter(ele => this.value === ele.type);
+  if(this.value !== 'all'){
+    print(itemsFilter ,eleShowIcon);
+  }else{
+    print(addColorObj ,eleShowIcon);
+  }
+});
+
+
+
+/**
+ * Funzione che stampa i filtri nell'html
+ * @param {array} array 
+ */
+function getElementHtmlFilter(array){
+  const itemsType = [];
+
+  array.forEach(ele => {
+    if(!itemsType.includes(ele.type)){
+      itemsType.push(ele.type);
+      document.getElementById('filter-selected').innerHTML += `<option value="${ele.type}">${ele.type}</option>`;
+    }
+  });
+}
